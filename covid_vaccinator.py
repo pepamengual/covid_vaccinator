1467a28b03c6be720ee9706c968f9daaecf2e3ed
Pep Amengual <pep@MacBook-Pro-de-Pep.local>
2020-05-21 11:22:26 +0200

    Reads a fasta file to pass to mhcflurry

--- covid_vaccinator.py
+++ covid_vaccinator.py
@@ -5,21 +5,21 @@
 from mhcflurry import Class1PresentationPredictor
 
 def load_predictor():
-    """
+    """ Returns an object
     Loads the mhcflurry predictor of class I alleles
     """
     predictor = Class1PresentationPredictor.load()
     return predictor
 
 def supported_alleles(predictor):
-    """
+    """ Returns a list
     Get all alleles that can be predicted with this mhcflurry version
     """
     alleles = predictor.supported_alleles
     return alleles
 
 def filter_human_alleles(alleles):
-    """
+    """ Returns a list
     Filter human alleles from all alleles: they start by "HLA-"
     Print how many human alleles can be predicted in each HLA family
     """
@@ -34,10 +34,32 @@ def filter_human_alleles(alleles):
         print("HLA-{} family with {} predictable alleles".format(group, len(allele_list)))
     return human_alleles
 
+def read_fasta(file_path):
+    """ Returns a dictionary
+    Converts a fasta file into a dictionary, where key is
+    the ID and the value is the sequence of the protein
+    """
+    data, names, sequences = {}, [], []
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith(">"):
+                name = line[1:]
+                names.append(name)
+            else:
+                sequences.append(line)
+    for name, sequence in zip(names, sequences):
+        data.setdefault(name, sequence)
+    return data
+
+#def predictor(fasta_data, alleles, threshold):
+
 def main():
+    fasta_file = "fasta.fa"
     predictor = load_predictor()
     alleles = supported_alleles(predictor)
     human_alleles = filter_human_alleles(alleles)
+    fasta_data = read_fasta(fasta_file)
 
 main()
 
